98c14cc5606bb3ef13436903cec6c1f1bc36a190
arul-1107 <arulrajvasahaa1107@gmail.com>
2026-05-28 18:58:18 +0530

    eight

--- metallic-frontend/src/components/Footer.jsx
+++ metallic-frontend/src/components/Footer.jsx
@@ -3,6 +3,6 @@ export default function Footer() {
   return (
     <footer className="p-10 text-center border-t border-gray-800">
       <p>© Zoro Metallic Portfolio</p>
-    </footer>N
+    </footer>
   )
 }
